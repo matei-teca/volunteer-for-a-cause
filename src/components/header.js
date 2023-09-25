e139b238bf49199714b458ade46695066893f7d4
Matei Teca <matei.teca@gmail.com>
2023-09-25 15:16:30 +0300

    refactor styles s1 header.js

--- src/components/header.js
+++ src/components/header.js
@@ -3,62 +3,12 @@ import { useState } from "react";
 import logoSrc from "../img/logo.jpg";
 import './styles.css';
 
-
-
- 
-
 function Header(){
 
-    const styles = {
-
-        containerA:{
-            padding: 20,
-            paddingTop: 10,
-            paddingBottom: 7,
-            // backgroundColor: "#EDC339",
-            // border: "3px solid black"
-
-        },
-
-        containerB: {
-            backgroundColor: "black",
-            color: "white",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-between",
-            alignItems: "center",
-            height: 320,    
-            padding: 30,
-            paddingTop: 0,
-            // backgroundImage: `url("https://destinations.com.ua/storage/shares/12_2018_01/NationalSymbols/Depositphotos_46882449_m-2015.jpg")`,
-            backgroundSize: "50%",
-            border: "3px solid #EDC339",
-            borderRadius: "5% 5% 5% 5%"
-
-        },
-
-        videoContainer: {
-            // backgroundColor: "green",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            width: "100%",
-            height: 350,
-            position: "absolute",
-            
-            
-        }
-        
-    }
-
-
-
-
-    var video = document.getElementById("video_headerId");
-    var video2 = document.getElementById("video2_headerId");
-    var video3 = document.getElementById("video3_headerId");
-    var videoContainer = document.getElementById("video_containerId");
-    var logo = document.getElementById("logoId");
+    let video = document.getElementById("video_headerId");
+    let video2 = document.getElementById("video2_headerId");
+    let videoContainer = document.getElementById("video_containerId");
+    let logo = document.getElementById("logoId");
 
     const [played, setPlayed] = useState(false);
 
@@ -74,7 +24,6 @@ function Header(){
         logo.classList.toggle("logoAfter");
      }
 
-
      function playVideo1(){
         setPlayed(!played);
 
@@ -82,7 +31,7 @@ function Header(){
             video.pause();
         } else{
             video.play();
-        }
+        }   
      }
 
      function playVideo2(){
@@ -96,10 +45,10 @@ function Header(){
      }
 
     return(
-        <div style={styles.containerA}> 
-        <div style={styles.containerB} >
-             <div style = {styles.videoContainer} id="video_containerId" className="video_container" onMouseEnter={showVideo} onMouseLeave={hideVideo}>
-                 
+        <div className="header_containerA"> 
+        <div className="header_containerB">
+
+            <div id="video_containerId" className="video_container" onMouseEnter={showVideo} onMouseLeave={hideVideo}>
             <video width="80%" height="350" className="video_header"  id="video_headerId" controls onClick={playVideo2}>
                 <source src={require('../Videos/video_header.mp4')} type="video/mp4"/>
 
@@ -108,23 +57,10 @@ function Header(){
                 <source src={require('../Videos/video_header.mp4')} type="video/mp4"/>
 
             </video>
-            {/* <video width="80%" height="350" className="video_header" id="video3_headerId">
-                <source src={require('../Videos/video_header.mp4')} type="video/mp4"/>
-
-            </video> */}
             </div>
-           
-            {/* <div style={{backgroundColor: "red"}}>
-            <a>
-               <div  style={{backgroundColor: "green"}}>
-
-               </div>
-           </a>
-            </div> */}
            
             <img src={logoSrc} alt="Logo" height={300} style={{borderRadius: "50%", marginTop: 30, marginLeft: 3.5}} id = "logoId" className="logo"></img>
 
-           
         </div>
         </div>
     )
