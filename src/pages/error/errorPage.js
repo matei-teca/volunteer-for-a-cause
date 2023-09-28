f04de5edd7dc45fb4bcbc56c76fe361d419c8098
Matei Teca <matei.teca@gmail.com>
2023-09-28 15:22:13 +0300

    fixed bug

--- src/pages/error/errorPage.js
+++ src/pages/error/errorPage.js
@@ -2,18 +2,16 @@
 import React, {useRef} from "react";
 import Header from "../../components/header";
 import Footer from "../../components/footer";
-
+import "./errorPage.css";
 
 function ErrorPage() {
 
   return (
     <div>      
-    <div className = "container">
+    <div className = "errorPage_container">
         <h1>ERROR page not found!</h1>
     </div>
     </div>
-
-
   );
 }
 
