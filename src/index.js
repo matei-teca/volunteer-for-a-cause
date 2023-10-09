8c19126430470b05272ee704d3aa269a4560fc71
Matei Teca <matei.teca@gmail.com>
2023-10-09 22:32:47 +0300

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { createStore } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import { Provider } from "react-redux";
+import { createStore } from "redux";    
 import allReducer from "./redux/reducers";
-import { Provider } from 'react-redux';
+import reportWebVitals from "./reportWebVitals";
 
-const store = createStore(allReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(
+  allReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
-ReactDOM.render( 
-<Provider store={store}>
-<App />
-</Provider>
-, document.getElementById('root') );
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
-// ReactDOM.render( 
-//   <App />
-//   , document.getElementById('root') );
-  
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// If you want to start measuring performance in your app
 reportWebVitals();
