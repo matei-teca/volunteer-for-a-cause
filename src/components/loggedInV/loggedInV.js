3eec438246f563119a421a87b1a31b1b78d0bde4
Matei Teca <matei.teca@gmail.com>
2023-10-09 21:54:23 +0300

    removed video feature ideea

--- src/components/loggedInV/loggedInV.js
+++ src/components/loggedInV/loggedInV.js
@@ -1,180 +1,153 @@
 import React from "react";
-import { useState } from 'react'; 
+import { useState } from "react";
 import { scroller } from "react-scroll";
-import './loggedInV.css';
-import '../styles.css';
+import "./loggedInV.css";
+import "../styles.css";
 
-function LogInV(){
+function LogInV() {
+  const [name, setName] = useState("");
+  const [contact, setContact] = useState("");
+  const [time, setTime] = useState("");
 
-    const [name, setName] = useState("");
-    const [contact, setContact] = useState("");
-    const [time, setTime] = useState("");
+  function title1TextAnimation() {
+    var elem = document.getElementById("title1Section");
+    var elemText = document.getElementById("title1SectionText");
+    var elemText2 = document.getElementById("title1SectionText2");
 
-    var video = document.getElementById("video_title1Id");
-    var title1 = document.getElementById("title1Id");
-    var container = document.getElementById("loggedIn_containerBId");
-    
+    elem.style.fontSize = "50px";
+    elemText.style.opacity = "1";
+    elemText2.style.opacity = "1";
+  }
 
-    function showVideo(){
-    //    video.play();
+  function changeTextColor() {
+    var text = document.getElementById("title1SectionText");
+    var text2 = document.getElementById("title1SectionText2");
 
-       video.classList.add("video_title1After");
-       video.classList.remove("video_title1");
+    text.classList.remove("title1SectionText");
+    text.classList.add("title1SectionTextHover");
+    text2.classList.remove("title1SectionText");
+    text2.classList.add("title1SectionTextHover");
+  }
 
-       title1.classList.add("title1After");
-       title1.classList.remove("title1");
+  function rechangeTextColor() {
+    var text = document.getElementById("title1SectionText");
+    var text2 = document.getElementById("title1SectionText2");
 
-       container.classList.add("loggedIn_containerBAfter");
-       container.classList.remove("loggedIn_containerB");
+    text.classList.remove("title1SectionTextHover");
+    text.classList.add("title1SectionText");
+    text2.classList.remove("title1SectionTextHover");
+    text2.classList.add("title1SectionText");
+  }
 
-       
-    }
+  const scrollToSection = () => {
+    scroller.scrollTo("scroll", {
+      duration: 800,
+      delay: 0,
+      smooth: "easeInOut",
+    });
 
-    function hideVideo(){
-        // video.pause();
+    console.log("woorks");
+  };
 
-        title1.classList.remove("title1After");
-        title1.classList.add("title1");
-
-        container.classList.remove("loggedIn_containerBAfter");
-       container.classList.add("loggedIn_containerB");
-     }
-
-     function title1TextAnimation(){
-         var elem = document.getElementById("title1Section");
-         var elemText = document.getElementById("title1SectionText");
-         var elemText2 = document.getElementById("title1SectionText2");
-
-         elem.style.fontSize = "50px";
-         elemText.style.opacity = "1";
-         elemText2.style.opacity = "1";
-     }
-
-     function changeTextColor () {
-
-      var text = document.getElementById("title1SectionText");
-      var text2 = document.getElementById("title1SectionText2");
-
-      text.classList.remove("title1SectionText");
-      text.classList.add("title1SectionTextHover");
-      text2.classList.remove("title1SectionText");
-      text2.classList.add("title1SectionTextHover");
-     }
-
-     function rechangeTextColor () {
-
-      var text = document.getElementById("title1SectionText");
-      var text2 = document.getElementById("title1SectionText2");
-
-      text.classList.remove("title1SectionTextHover");
-      text.classList.add("title1SectionText");
-      text2.classList.remove("title1SectionTextHover");
-      text2.classList.add("title1SectionText");
-     }
- 
-     const scrollToSection = () => {
-        scroller.scrollTo("scroll", {
-          duration: 800,
-          delay: 0,
-          smooth: "easeInOut",
-        });
-
-        console.log("woorks")
-      };
-
-    return(
-        <div className="loggedIn_containerA">
-        <div className="loggedIn_containerB" id="loggedIn_containerBId" >
-        
-        <article id="title1Id" className="title1" onMouseEnter={title1TextAnimation} >
-            
-            <h1 className="textStyle title1Section" id="title1Section">
-                Bine ai venit!
-            </h1>
+  return (
+    <div className="loggedIn_containerA">
+      <div className="loggedIn_containerB" id="loggedIn_containerBId">
+        <article
+          id="title1Id"
+          className="title1"
+          onMouseEnter={title1TextAnimation}
+        >
+          <h1 className="textStyle title1Section" id="title1Section">
+            Bine ai venit!
+          </h1>
 
           <p className="title1SectionText" id="title1SectionText">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla dapibus, ipsum ut tempor maximus, arcu turpis accumsan eros, 
-            eu elementum est quam non nisi. Etiam magna velit, faucibus quis fringilla et, placerat vel erat. 
-            Proin convallis arcu sed fermentum feugiat. Curabitur volutpat dui in justo vulputate scelerisque. 
-            Nulla non dui at purus scelerisque mollis et ac sapien. Pellentesque ultricies ante a metus sagittis feugiat. 
-            Pellentesque convallis risus nisi, ut ornare orci ullamcorper vitae. Phasellus venenatis ullamcorper mauris, quis tristique libero ullamcorper non. 
-            Mauris euismod imperdiet felis id feugiat. 
-
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            dapibus, ipsum ut tempor maximus, arcu turpis accumsan eros, eu
+            elementum est quam non nisi. Etiam magna velit, faucibus quis
+            fringilla et, placerat vel erat. Proin convallis arcu sed fermentum
+            feugiat. Curabitur volutpat dui in justo vulputate scelerisque.
+            Nulla non dui at purus scelerisque mollis et ac sapien. Pellentesque
+            ultricies ante a metus sagittis feugiat. Pellentesque convallis
+            risus nisi, ut ornare orci ullamcorper vitae. Phasellus venenatis
+            ullamcorper mauris, quis tristique libero ullamcorper non. Mauris
+            euismod imperdiet felis id feugiat.
           </p>
           <p className="title1SectionText" id="title1SectionText2">
-            Donec condimentum lectus sit amet sapien auctor ullamcorper. 
-            Aliquam erat volutpat. Integer ultricies rhoncus nunc nec luctus. 
-            Fusce porttitor diam id egestas pretium. Nam imperdiet mauris ut hendrerit elementum. 
-            Suspendisse vel orci at odio lacinia fringilla quis a ex. 
-            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; 
-            Integer diam leo, fermentum id elit eget, rhoncus lacinia felis. Morbi leo lacus, consequat id lacus quis, faucibus sodales enim.
-            </p>
-
-           
+            Donec condimentum lectus sit amet sapien auctor ullamcorper. Aliquam
+            erat volutpat. Integer ultricies rhoncus nunc nec luctus. Fusce
+            porttitor diam id egestas pretium. Nam imperdiet mauris ut hendrerit
+            elementum. Suspendisse vel orci at odio lacinia fringilla quis a ex.
+            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+            posuere cubilia curae; Integer diam leo, fermentum id elit eget,
+            rhoncus lacinia felis. Morbi leo lacus, consequat id lacus quis,
+            faucibus sodales enim.
+          </p>
         </article>
+      </div>
 
-        <video width="80%" height="350" className="video_title1"  id="video_title1Id" onMouseEnter={showVideo} onMouseLeave={hideVideo} controls>
-                <source src={require('../..//videos/video_header.mp4')} type="video/mp4"/>
-
-            </video>
-        </div>
-
-      
-        <section className="title2" onClick={scrollToSection} onMouseEnter={changeTextColor} onMouseLeave={rechangeTextColor}>
-
+      <section
+        className="title2"
+        onClick={scrollToSection}
+        onMouseEnter={changeTextColor}
+        onMouseLeave={rechangeTextColor}
+      >
         <p className="llwl">
-              <abbr>LLWL</abbr> (Live a life worth living!)
-            </p>
+          <abbr>LLWL</abbr> (Live a life worth living!)
+        </p>
 
-        <h1 className="textStyle" style={{fontSize: 35}}>
-            Join the battle!
+        <h1 className="textStyle" style={{ fontSize: 35 }}>
+          Join the battle!
         </h1>
 
         <p className="textStyle scroll">
-        Let us now when are you available to participate in volunteer activities, and we will hook you up!
+          Let us now when are you available to participate in volunteer
+          activities, and we will hook you up!
         </p>
-        </section>
+      </section>
 
+      <article className="formV">
+        <form
+          className="textStyle"
+          onSubmit={() => alert("your answer was succesfully submited")}
+        >
+          <label className="form_section1">
+            Enter your name:
+            <input
+              className="form_input"
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
 
-        <article className="formV">
-        <form className="textStyle"  onSubmit = {() => alert("your answer was succesfully submited")}>
-      
-      <label className="form_section1">Enter your name:
-        <input
-        className="form_input"
-          type="text" 
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </label>
+          <label className="form_section">
+            Phone / email:
+            <input
+              className="form_input"
+              type="text"
+              value={contact}
+              onChange={(e) => setContact(e.target.value)}
+            />
+          </label>
 
-      <label className="form_section">Phone / email:
-        <input
-        className="form_input"
-          type="text" 
-          value={contact}
-          onChange={(e) => setContact(e.target.value)}
-        />
-      </label>
+          <label className="form_section">
+            Time when available
+            <input
+              className="form_input"
+              type="text"
+              value={time}
+              onChange={(e) => setTime(e.target.value)}
+            />
+          </label>
 
-      <label className="form_section">Time when available
-        <input
-        className="form_input"
-          type="text" 
-          value={time}
-          onChange={(e) => setTime(e.target.value)}
-        />
-      </label>
-
-      <div className="input_wrap ">
-      <input className="submitBttn" type="submit" value="Submit" ></input>
-      </div>
-      
-    </form>
-    </article>
-
-        </div>
-    )
+          <div className="input_wrap ">
+            <input className="submitBttn" type="submit" value="Submit"></input>
+          </div>
+        </form>
+      </article>
+    </div>
+  );
 }
 
 export default LogInV;
