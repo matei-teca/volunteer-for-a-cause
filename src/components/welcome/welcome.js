597d07538770010f6fe396ffc19d6ea4631ba241
Matei Teca <matei.teca@gmail.com>
2023-10-10 17:35:23 +0300

    fixed animation

--- src/components/welcome/welcome.js
+++ src/components/welcome/welcome.js
@@ -4,19 +4,18 @@ import "./welcome.css";
 
 function Welcome() {
 
-  function playAnimation() {
+  setTimeout(() => {
     let container = document.getElementById("welcome_container");
     let text = document.getElementById("welcome_text");
 
     container.className = "welcome_container_large";
     text.className = "welcome_text_large textStyle";
-  }
+  }, 1000);
 
   return (
     <div
       id="welcome_container"
       className="welcome_container_small"
-      onMouseEnter={playAnimation}
     >
       <h1 id="welcome_text" className="welcome_text_small textStyle">
         Log in to start contributing
