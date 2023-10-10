f0c0128f904f37629d63dc01bbd019458a3060de
Matei Teca <matei.teca@gmail.com>
2023-10-10 21:22:05 +0300

    upgraded project routing structure

--- src/index.js
+++ src/index.js
@@ -1,23 +1,46 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import { Provider } from "react-redux";
 import { createStore } from "redux";    
 import allReducer from "./redux/reducers";
-import reportWebVitals from "./reportWebVitals";
+import LandingPage from "./pages/landing/landingPage";
+import ContactPage from "./pages/contact/contactPage";
+import ErrorPage from "./pages/error/errorPage";
+import LegalPage from "./pages/legal/legalPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <LandingPage />,
+      },
+      {
+        path: "/contact",
+        element: <ContactPage />,
+      },
+      {
+        path: "/legal",
+        element: <LegalPage />,
+      }
+    ]
+  }
+]);
 
 const store = createStore(
   allReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app
-reportWebVitals();
