43b71bce770d38af38f01e7a0ab27c4a3cc1cce9
Matei Teca <matei.teca@gmail.com>
2023-10-24 17:15:29 +0300

    responsive: landing page

--- src/components/welcome/welcome.js
+++ src/components/welcome/welcome.js
@@ -9,8 +9,8 @@ function Welcome() {
     let text2 = document.getElementById("welcome_text2");
 
     container.className = "welcome_container welcome_container_large";
-    text.className = "welcome_text_large";
-    text2.className = "welcome_text_large";
+    text.className = "welcome_text welcome_text_large";
+    text2.className = "welcome_text welcome_text_large";
   }, 1000);
 
   return (
@@ -18,10 +18,10 @@ function Welcome() {
       id="welcome_container"
       className="welcome_container welcome_container_small"
     >
-      <div id="welcome_text" className="welcome_text_small">
+      <div id="welcome_text" className="welcome_text welcome_text_small">
         Welcome! 
       </div>
-      <div id="welcome_text2" className="welcome_text_small">
+      <div id="welcome_text2" className="welcome_text welcome_text_small">
         How do you wish to proceed?
       </div>
     </div>
