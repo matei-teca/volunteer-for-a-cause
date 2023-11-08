c4105a994a1ca79284ec8b9885d3a1456211bea4
Matei Țeca <116873451+matei-teca@users.noreply.github.com>
2023-11-08 19:36:55 +0200

    undo uploading server

--- server/src/main/java/com/codecool/database/UserDataRepository.java
+++
@@ -1,8 +0,0 @@
-package com.codecool.database;
-
-import com.codecool.data.UserData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDataRepository extends JpaRepository<UserData, Long> {
-    // Sa nu uit sa am pgAdmin deschis
-}
